f54bc589e3bf17a3ad9f505e7f7f4aea3a248cc3
Grégoire de Massé <gregoire@cardiologs.com>
2023-12-14 00:20:19 +0100

    day 13 part 2

--- day13.ts
+++ day13.ts
@@ -18,40 +18,41 @@ const test_input = `#.##..##.
 function solve(text: string) {
     return text.split("\n\n").map((block, idx) => {
 
-        let lines = block.split("\n")
+        let lines = block.split("\n").map((line) => line.split(""))
         for (let index = 0; index < lines.length - 1; index++) {
-            let is_mirror = true
+            let smudge = 0
             for (let i=0;i<lines.length;i+=1) {
                 if (index - i < 0 || index + i + 1 >= lines.length) {
                     break
                 }
-                if (lines[index - i] !== lines[index + i + 1]) {
-                    is_mirror = false
-                }
+                const count_diff = lines[index - i].reduce((acc, val, cidx) => acc + (val != lines[index + i + 1][cidx]), 0)
+                // console.log(count_diff, lines[index - i], lines[index + i + 1])
+                smudge += count_diff
             }
-            if (is_mirror) {
+            // console.log(index, smudge)
+            if (smudge == 1) {
                 return (index + 1) * 100
             }
         }
         let lines_transposed = []
         let ll = lines[0].length
         for (let index = 0; index < ll; index++) {
-            lines_transposed.push(lines.map((line) => line[index]).join(""))
+            lines_transposed.push(lines.map((line) => line[index]))
         }
         // console.log(lines)
         // console.log(lines_transposed)
         lines = lines_transposed
         for (let index = 0; index < lines.length - 1; index++) {
-            let is_mirror = true
+            let smudge = 0
             for (let i=0;i<lines.length;i+=1) {
                 if (index - i < 0 || index + i + 1 >= lines.length) {
                     break
                 }
-                if (lines[index - i] !== lines[index + i + 1]) {
-                    is_mirror = false
-                }
+                const count_diff = lines[index - i].reduce((acc, val, cidx) => acc + (val != lines[index + i + 1][cidx]), 0)
+                // console.log(count_diff, lines[index - i], lines[index + i + 1])
+                smudge += count_diff
             }
-            if (is_mirror) {
+            if (smudge == 1) {
                 return index + 1
             }
         }
